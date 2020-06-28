b99ac97a40111cba8d56908c18eef9c412180ee7
marciano <marciano@debian>
2020-06-28 11:01:12 -0300

    versao0.0

--- app/src/main/java/br/com/unipar/auxilioenergencial/Pessoa.java
+++ app/src/main/java/br/com/unipar/auxilioenergencial/Pessoa.java
@@ -67,6 +67,7 @@ public class Pessoa{
         Calendar today = Calendar.getInstance();
 
         // estrai ano, mes, dia
+        // mais 1 nos meses é devido eles  serem indexados a partir de 0, Janeiro, e Dezembro, 11.
         int ano = today.get(Calendar.YEAR);
         int mesInt = dateOfBirth.get(Calendar.MONDAY) + 1;
         int diaInt  = dateOfBirth.get(Calendar.DAY_OF_MONTH) + 20;
